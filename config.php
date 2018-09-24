bd36ab7bde31e8f5a0443c8f8cf3f7b1da38d3c9
kishore <kishorerk2@gmail.com>
2018-09-24 23:14:07 +0530

    code added

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+/*
+// mysql_connect("database-host", "username", "password")
+$conn = mysql_connect("localhost","root","root") 
+			or die("cannot connected");
+
+// mysql_select_db("database-name", "connection-link-identifier")
+@mysql_select_db("test",$conn);
+*/
+
+/**
+ * mysql_connect is deprecated
+ * using mysqli_connect instead
+ */
+
+$databaseHost = '13.232.252.61';
+$databaseName = 'test';
+$databaseUsername = 'root';
+$databasePassword = 'admin';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+?>
